73111606fda3d17e642b9ff7c8714e3fa40d616e
狼的小窝 <59323300+AloneScar@users.noreply.github.com>
2022-08-02 22:07:42 +0800

    design index.js, use flex, it is a good layout tools, make me love css

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# als-logs
+# als-records
 AloneScar Personal Recording
+
+## Introduction
+* Include android web desktop application, will support all device
+* Can record text, picture, video, audio
+* Having the majority of classify, such as link,diary, note, blog and so on, but only designed personally by alonescar
+
+## Environment && Dependence
+Environement: Python Nodejs
+
+Python Package: pipenv flask flask-sqlalchemy
+
+Nodejs Package: marked
+
+## Todo
+1. [x] Create python virtual environment
+    * [x] Turn pipenv download url to tsinghua
+2. Design flask file struct
+3. Design headed pages
+4. Design database
+5. Write flask code to show the page on web
---
+++ records/templates/index.js
@@ -0,0 +1,34 @@
+const c1 = document.querySelector('#c1 .shortText');
+const c2 = document.querySelector('#c2 .shortText');
+const c3 = document.querySelector('#c3 .shortText');
+const c4 = document.querySelector('#c4 .shortText');
+
+c1.innerHTML = `
+    * Include android web desktop application, will support all device
+    * Can record text, picture, video, audio
+    * Having the majority of classify, such as link,diary, note, blog and so on, but only designed personally by alonescar
+`
+c2.innerHTML = `
+    Environement: Python Nodejs
+
+    Python Package: pipenv flask flask-sqlalchemy
+
+    Nodejs Package:
+`
+c3.innerHTML = `
+    1. [x] Create python virtual environment
+        * [x] Turn pipenv download url to tsinghua
+    2. Design flask file struct
+    3. Design headed pages
+    4. Design database
+    5. Write flask code to show the page on web
+`
+
+c4.innerHTML = `
+    1. [x] Create python virtual environment
+        * [x] Turn pipenv download url to tsinghua
+    2. Design flask file struct
+    3. Design headed pages
+    4. Design database
+    5. Write flask code to show the page on web
+`
